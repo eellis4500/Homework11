0bc98371be3e5e191d696e9d1fa8b4639c5c22ae
VikingFrog <eellis4500@gmail.com>
2021-04-22 20:25:49 -0400

    first commit w/ server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const apiRoutes = require('./routes/api');
+const htmlRoutes = require('./routes/html');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use("/api", apiRoutes)
+// app.use("/", htmlRoutes)
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
